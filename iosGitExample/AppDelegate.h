991a70f0545c619a9eb98bec1e5a4d55a966bcc0
bcarson <bcarson408@gmail.com>
2015-04-22 14:33:07 -0700

    Initial Commit

---
+++ iosGitExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iosGitExample
+//
+//  Created by Brant Carson on 4/22/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iosGitExampleTests/iosGitExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  iosGitExampleTests.m
+//  iosGitExampleTests
+//
+//  Created by Brant Carson on 4/22/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iosGitExampleTests : XCTestCase
+
+@end
+
+@implementation iosGitExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
